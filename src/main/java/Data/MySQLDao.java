c6464cac7ac2928515efc9244b586c5f1cf3b71d
Weston Dransfield <wdrans@gmail.com>
2015-03-19 14:36:54 -0600

    db access working locally

--- src/main/java/Data/MySQLDao.java
+++ src/main/java/Data/MySQLDao.java
@@ -15,24 +15,30 @@ import java.sql.*;
  */
 public class MySQLDao implements SoulDao {
 
-    private String dbUrl = "jdbc:mysql://localhost/gitcoupled";
-    private String user = "Gandalf";
-    private String password = "puremagic";
+    private String dbUrl = "jdbc:mysql://localhost:8889/gitcoupled";
+    private String user = "root";
+    private String password = "pass";
     private Connection conn = null;
     private Statement statement = null;
-    
+
     public MySQLDao() {
         try {
-            
+
             Class.forName("com.mysql.jdbc.Driver");
             conn = DriverManager.getConnection(dbUrl, user, password);
+
             statement = conn.createStatement();
-            
+
         } catch (Exception ex) {
+            System.out.println("=======================================");
             ex.printStackTrace();
+            System.out.println(dbUrl);
+            System.out.println(user);
+            System.out.println(password);
+            System.out.println("++++++++++++++++++++++++++++++++++++++++");
         }
     }
-    
+
     @Override
     public void addUser(User user) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -40,28 +46,27 @@ public class MySQLDao implements SoulDao {
 
     @Override
     public User getUser(String username) {
-        
+
         User user = null;
-        
+
         try {
-            String sql = "SELECT * FROM user WHERE username = '" + username + "'";
+            String sql = "SELECT * FROM user WHERE github_username = '" + username + "'";
             ResultSet results = statement.executeQuery(sql);
-            
+
+            results.first();
             int id = results.getInt("id");
             String gender = results.getString("gender");
             int age = results.getInt("age");
             String github_username = results.getString("github_username");
             String quote = results.getString("quote");
             String bio = results.getString("bio");
-            
-            
+
             user = new User(id, gender, age, github_username, quote, bio);
-            
-        }
-        catch (Exception ex) {
+
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
-        
+
         return user;
     }
 
@@ -84,5 +89,5 @@ public class MySQLDao implements SoulDao {
     public List<User> getUsers(String primary) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
