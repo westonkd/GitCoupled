de2e28b2a85de61af7e09fe3dc96135c89c4dcf4
Weston Dransfield <wdrans@gmail.com>
2015-03-19 10:18:04 -0600

    moved some classes into a new package and refactored

--- src/main/java/UI/CallBack.java
+++ src/main/java/UI/CallBack.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Application;
+package UI;
 
 import Data.HttpConnection;
 import com.sun.corba.se.spi.activation.Repository;
--- src/main/java/UI/PersonalProfile.java
+++ src/main/java/UI/PersonalProfile.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Application;
+package UI;
 
 import java.io.IOException;
 import java.io.PrintWriter;
