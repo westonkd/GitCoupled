6629fffff94a82a3b9afb245fb70b036c685dfbf
smckaysmalley <smckaysmalley@gmail.com>
2015-03-28 08:47:30 -0600

    test

--- src/main/java/UI/Profile.java
+++ src/main/java/UI/Profile.java
@@ -69,7 +69,7 @@ public class Profile extends HttpServlet {
                 request.getSession().setAttribute("user", user);
                 
                 //forward
-                request.getRequestDispatcher("profile.jsp").forward(request, response);   
+                //request.getRequestDispatcher("profile.jsp").forward(request, response);   
                 
             } else {        
                 out.println(github.getMyself().getLogin() + "does not exists");
@@ -84,7 +84,7 @@ public class Profile extends HttpServlet {
                 request.getSession().setAttribute("user", user);
                 
                 //forward
-                request.getRequestDispatcher("edit-profile.jsp").forward(request, response);   
+                //request.getRequestDispatcher("edit-profile.jsp").forward(request, response);   
             }
         }
     }
