9ad0f4e996e41f9433f933cebf6d30bb1c7303ef
Weston Dransfield <wdrans@gmail.com>
2015-04-01 18:57:30 -0600

    test to delete messages

--- src/main/java/Application/DeleteMessage.java
+++ src/main/java/Application/DeleteMessage.java
@@ -21,7 +21,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Weston
  */
-@WebServlet(name = "ReplyToMessage", urlPatterns = {"/ReplyToMessage"})
+@WebServlet(name = "ReplyToMessage", urlPatterns = {"/DeleteMessage"})
 public class DeleteMessage extends HttpServlet {
 
     /**
